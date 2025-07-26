2fc5866f2fb2a8ba31921d0e12aeff112ca0cb21
김혜주 <sueelly@naver.com>
2025-07-26 14:59:19 +0900

    init: Create spring project

---
+++ src/main/java/com/eight/memory_garden/MemoryGardenApplication.java
@@ -0,0 +1,13 @@
+package com.eight.memory_garden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryGardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryGardenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eight/memory_garden/MemoryGardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eight.memory_garden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryGardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
